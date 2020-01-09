52f7691e6e23dc4bd9c241004cd50e5c24d598a3
06wj <06wj@163.com>
2020-01-09 12:41:17 +0800

    test: update GLTFParser test

--- test/spec/loader/GLTFParser.test.js
+++ test/spec/loader/GLTFParser.test.js
@@ -10,16 +10,34 @@ describe('GLTFParser', () => {
     it('register & unregister ExtensionHandler', () => {
         const parser = new GLTFParser;
         should(parser.getExtensionHandler('hello')).be.undefined();
-        
+
         GLTFParser.registerExtensionHandler('hello', {
-            parse(){
+            parse() {
 
             }
         });
         should(parser.getExtensionHandler('hello')).not.be.undefined();
         should(parser.getExtensionHandler('hello2')).be.undefined();
-        
+
         GLTFParser.unregisterExtensionHandler('hello');
         should(parser.getExtensionHandler('hello')).be.undefined();
+    });
+
+    it('getImageType', () => {
+        const parser = new GLTFParser();
+        parser.json = {
+            images: [{
+                mimeType: 'image/jpeg'
+            }, {
+                mimeType: 'image/ktx'
+            }, {
+                mimeType: 'image/hdr'
+            }, {}, null]
+        };
+        parser.getImageType(0).should.equal('');
+        parser.getImageType(1).should.equal('ktx');
+        parser.getImageType(2).should.equal('');
+        parser.getImageType(3).should.equal('');
+        parser.getImageType(4).should.equal('');
     });
 });
