54c0c7494fea11240ab277d7959bf769b080ced9
06wj <06wj@163.com>
2020-07-17 17:11:44 +0800

    fix lod bug

--- src/shader/method/textureEnvMap.glsl
+++ src/shader/method/textureEnvMap.glsl
@@ -7,10 +7,12 @@ vec4 textureEnvMap(samplerCube texture, vec3 position){
 }
 
 vec4 textureEnvMapIncludeMipmapsLod(sampler2D texture, vec3 position, float lod){
+    lod = floor(lod);
     vec2 uv = vec2(atan(position.x, position.z) * HILO_INVERSE_PI * 0.5+0.5,  acos(position.y) * HILO_INVERSE_PI);
-    
+
     float scale = pow(2.0, lod);
-    return texture2D(texture, vec2(uv.x / scale, (uv.y / scale) + 1.0 - 1.0/pow(2.0, lod)));
+
+    return texture2D(texture, vec2(uv.x / scale, (uv.y / scale / 2.0) + 1.0 - 1.0/pow(2.0, lod)));
 }
 
 #ifdef HILO_USE_SHADER_TEXTURE_LOD
